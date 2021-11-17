f34b7c3d5ca1667e5e4f2edaa25f4ce6a23c2132
Kenneth Adjei-mensah <kenadjei20@gmail.com>
2021-11-17 22:16:56 +0000

    app js

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, ScrollView } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Woman Item</Text>
-
-      <Text style={styles.list}>Item 1</Text>
-      <StatusBar style="auto" />
-      <Text style={styles.list}>Item 2</Text>
-      <Text style={styles.list}>Item 3</Text>
-      <Text style={styles.list}>Item 4</Text>
+      <ScrollView>
+        <View style={styles.header}>
+          <Text>Woman Item</Text>
+        </View>
+        <TextInput style={styles.text} />
+        <View style={styles.items}>
+          <Text style={styles.item1}>Item 1</Text>
+          <StatusBar style="auto" />
+          <Text style={styles.item2}>Item 2</Text>
+          <Text style={styles.item3}>Item 3</Text>
+          <Text style={styles.item4}>Item 4</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -25,9 +31,8 @@ const styles = StyleSheet.create({
   },
   header: {
     marginTop: 100,
+    marginBottom: 100,
 
-    marginRight: 200,
-    paddingRight: 0,
     backgroundColor: "blue",
     flex: 1,
     flexDirection: "row",
@@ -36,11 +41,20 @@ const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: "bold",
   },
-  list: {
+  item: {
     flex: 1,
-    flexDirection: "row",
     marginBottom: 2,
     textAlign: "left",
+    backgroundColor: "yellow",
+    width: 400,
+  },
+  text: {
+    marginTop: 5,
+    marginBottom: 100,
+    marginLeft: 70,
+    marginRight: 20,
+    borderStyle: "solid",
+    borderColor: "black",
     backgroundColor: "yellow",
   },
 });
